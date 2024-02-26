3834e84e3007dc81e26dfa4450c44361a5870015
Amanuael Kebede <66178225+Manuel-heav@users.noreply.github.com>
2024-02-26 09:19:32 +0300

    Add solution for Minimum Common Value

---
+++ Solutions/leetcode/minimum-common-value.py
@@ -0,0 +1,8 @@
+class Solution:
+    def getCommon(self, nums1: List[int], nums2: List[int]) -> int:
+        common = set(nums1).intersection(set(nums2))
+        if len(common) == 0:
+            return -1
+        else:
+            return min(common)
+        
